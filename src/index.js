6ffcc22c5d6218473f6f15f892b35f98b1b1d822
helenilson oliveira <helenilsoon@gmail.com>
2021-08-24 11:52:17 -0400

    app react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <div>welcome Website  :)</div>,     
+const lista=[
+  {
+    id:"1",
+    name:"helenilson"
+  },
+  {
+    id:2,
+    name:"jussara"
+  }
+]
 
+function Pessoas(props) {
+  return (
+    <div>
+     {
+      lista.map(
+        (item, index) =>(
+          <p>{index}:{item.name}</p>,
+          <p>{index}:{item.id}</p>
+          
+          )
+        )
+     }
+    </div>
+  )
+}
+
+function Pessoa(props) {
+  return(
+    <div>
+      {props.tipodedado}:{lista[props.index].name}
+    </div>
+  )
+}
+ReactDOM.render(
+        <div>
+            <Pessoas />
+            <Pessoa tipodedado="nome" index={0} />
+        </div>,
     document.getElementById("root")
 )
